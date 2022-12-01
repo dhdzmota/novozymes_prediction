6939fc8455382f2385718eb2029e8f032a4a9f46
dhdzmota <dhdzmota@gmail.com>
2022-12-01 01:09:25 -0600

    Create Cookiecutter structure repo. Included initial files from notebooks.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project tries to solve the Kaggle problem named: Novozymes Enzyme Stability Prediction.',
+    author='dhdmzmota',
+    license='MIT',
+)
